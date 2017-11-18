f666e3a266a65a46626f9e79320eec01877e2b67
David Rowan III <rowand823@gmail.com>
2017-11-18 14:10:25 -0600

    Add files via upload

---
+++ dist/main.bundle.js
@@ -0,0 +1,165 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_lazy_route_resource lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "\r\n\r\nh1 {\r\n  text-align: center;\r\n  color: #d8bc61;\r\n  font-family: 'Cinzel', serif;\r\n  font-weight: bold;\r\n}\r\n\r\nh2 {\r\n  text-align: center;\r\n}\r\n\r\n\r\n#smallText {\r\n  font-family: 'Coda', cursive;\r\n  font-size: .3em;\r\n  color: #fff;\r\n}\r\n\r\n.navbar-brand {\r\n  font-family: 'Cinzel', serif;\r\n}\r\n\r\n.jumbotron-billboard {\r\n  width: 100%;\r\n  height: 900px;\r\n  background-image: url('/assets/images/laptop.jpeg');\r\n  background-size: cover;\r\n  background-repeat: no-repeat;\r\n\r\n}\r\n.social-area {\r\n  margin-bottom: 20px;\r\n}\r\n\r\n.fa {\r\n  color: rgb(12, 12, 14);\r\n}\r\n\r\n#progIntro {\r\n  background: linear-gradient(-90deg, rgba(245, 208, 165, 0.384), rgba(211, 159, 91, 0.664));\r\n}\r\n\r\n#aboutInfo {\r\n  background: linear-gradient(135deg, #58503959 0%,rgba(252,205,77,1) 50%,rgba(248, 178, 0, 0.548) 82%,rgba(251, 221, 147, 0.658) 94%);\r\n}\r\n\r\n\r\n\r\n\r\n\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<nav class=\"navbar navbar-expand-md fixed-top navbar-dark bg-dark\">\r\n    <a class=\"navbar-brand text-warning\" href=\"#\">DR3WEBDEV</a>\r\n    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\"\r\n     aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n      <span class=\"navbar-toggler-icon\"></span>\r\n    </button>\r\n    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\r\n      <ul class=\"navbar-nav mr-auto\">\r\n        <li class=\"nav-item active\">\r\n          <a class=\"nav-link text-primary\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\r\n        </li>\r\n        <li class=\"nav-item\">\r\n          <a class=\"nav-link\" href=\"#\">Link</a>\r\n        </li>\r\n        <li class=\"nav-item dropdown\">\r\n          <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\"\r\n          aria-expanded=\"false\">Dropdown</a>\r\n          <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\r\n            <a class=\"dropdown-item\" href=\"#\">Action</a>\r\n            <a class=\"dropdown-item\" href=\"#\">More Action</a>\r\n            <div class=\"dropdown-divider\"></div>\r\n            <a class=\"dropdown-item\" href=\"#\">Some More Action</a>\r\n          </div>\r\n        </li>\r\n        <li class=\"nav-item\">\r\n          <a class=\"nav-link disable\" href=\"#\">Disabled</a>\r\n        </li>\r\n      </ul>\r\n      <form class=\"form-inline my-2 my-lg-0\">\r\n        <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">\r\n        <button class=\"btn btn-outline-warning my-2 my-sm-0\" type=\"submit\">Search</button>\r\n      </form>\r\n    </div>\r\n  </nav>\r\n\r\n  <!-- End of the navigation section -->\r\n\r\n\r\n  <div class=\"jumbotron jumbotron-billboard bg-dark text-white\">\r\n    <h1 class=\"text-warning display-3\">DR3/WEB/DEV<br/><small class=\"text-primary\" id=\"smallText\">ANGULAR DEVELOPMENT</small></h1>\r\n  </div>\r\n  <div class=\"container text-muted\">\r\n      <div class=\"row social-area\">\r\n        <div class=\"col-md-4 col-sm-12\">\r\n          <i class=\"fa fa-free-code-camp\" aria-hidden=\"true\"></i>\r\n            <h3>Free Code Camp</h3>\r\n            <p>Pellentesque in ipsum id orci porta dapibus. Curabitur arcu erat, accumsan id imperdiet et,\r\n            porttitor at sem. Curabitur aliquet quam id dui posuere blandit.</p>\r\n        </div>\r\n          <div class=\"col-md-4 col-sm-12\">\r\n            <i class=\"fa fa-github\" aria-hidden=\"true\"></i>\r\n              <h3>GitHub</h3>\r\n              <p>Pellentesque in ipsum id orci porta dapibus. Curabitur arcu erat, accumsan id imperdiet et,\r\n              porttitor at sem. Curabitur aliquet quam id dui posuere blandit.</p>\r\n          </div>\r\n          <div class=\"col-md-4 col-sm-12\">\r\n            <i class=\"fa fa-youtube\" aria-hidden=\"true\"></i>\r\n              <h3>YouTube</h3>\r\n              <p>Pellentesque in ipsum id orci porta dapibus. Curabitur arcu erat, accumsan id imperdiet et,\r\n               porttitor at sem. Curabitur aliquet quam id dui posuere blandit.</p>\r\n          </div>\r\n        </div>\r\n  </div>\r\n          <!-- end of Social Section -->\r\n\r\n  <div class=\"container-fluid text-center text-muted px-5 py-5\" id=\"progIntro\">\r\n        <h3 class=\"display-3 mb-5\" id=\"angProHeading\">Angular Programming</h3>\r\n      <div class=\"row\">\r\n        <div class=\"card col-md-4 col-sm-12\">\r\n          <div class=\"card-body\">\r\n            <h4 class=\"card-title\">Intro Text</h4>\r\n            <p>Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi,\r\n              pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada.</p>\r\n              <a href=\"#\" class=\"btn btn-outline-warning\">Click for the image</a>\r\n          </div>\r\n        </div>\r\n        <div class=\"card col-md-4 col-sm-12\">\r\n          <div class=\"card-body\">\r\n            <h4 class=\"card-title\">Intro Text</h4>\r\n            <p>Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi,\r\n              pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada.</p>\r\n              <a href=\"#\" class=\"btn btn-outline-warning\">Click for the image</a>\r\n          </div>\r\n        </div>\r\n        <div class=\"card col-md-4 col-sm-12\">\r\n          <div class=\"card-body\">\r\n            <h4 class=\"card-title\">Intro Text</h4>\r\n            <p>Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi,\r\n              pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada.</p>\r\n              <a href=\"#\" class=\"btn btn-outline-warning\">Click for the image</a>\r\n          </div>\r\n        </div>\r\n      </div>\r\n  </div>\r\n          <!--About the Team Section -->\r\n  <div class=\"container-fluid text-center text-muted px-5 py-5\" id=\"aboutInfo\">\r\n      <h3 class=\"display-4\">About The Team</h3>\r\n      <nav class=\"nav nav-pills mt-5 text-center flex-column flex-md-row\" id=\"pills-tab\" role=\"tablist\">\r\n        <a class=\"nav-item nav-link active\" id=\"nav-home-tab\" data-toggle=\"tab\" href=\"#nav-home\" role=\"tab\" aria-controls=\"nav-home\" aria-selected=\"true\">Von R.</a>\r\n        <a class=\"nav-item nav-link\" id=\"nav-profile-tab\" data-toggle=\"tab\" href=\"#nav-profile\" role=\"tab\" aria-controls=\"nav-profile\" aria-selected=\"false\">David R.</a>\r\n        <a class=\"nav-item nav-link\" id=\"nav-contact-tab\" data-toggle=\"tab\" href=\"#nav-contact\" role=\"tab\" aria-controls=\"nav-contact\" aria-selected=\"false\">Ashanti B.</a>\r\n      </nav>\r\n      <div class=\"tab-content\" id=\"nav-tabContent\">\r\n        <div class=\"tab-pane fade show active\" id=\"nav-home\" role=\"tabpanel\" aria-labelledby=\"nav-home-tab\">\r\n          <p>Donec sollicitudin molestie malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.\r\n             Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>\r\n        </div>\r\n        <div class=\"tab-pane fade\" id=\"nav-profile\" role=\"tabpanel\" aria-labelledby=\"nav-profile-tab\">\r\n          <p>Donec sollicitudin molestie malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.\r\n             Cras ultricies ligula sed magna dictum porta. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>\r\n        </div>\r\n        <div class=\"tab-pane fade\" id=\"nav-contact\" role=\"tabpanel\" aria-labelledby=\"nav-contact-tab\">\r\n          <p>Donec sollicitudin molestie malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.\r\n             Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>\r\n        </div>\r\n      </div>\r\n  </div>\r\n\r\n              <!--End of The About Team Section -->\r\n    <div class=\"container-fluid bg-dark text-white px-5 py-5\">\r\n      <h3 class=\"text-center text-muted display-3\">Contact Us!</h3>\r\n      <form>\r\n          <div class=\"form-row\">\r\n              <div class=\"form-group col-md-6\">\r\n                <label for=\"inputEmail4\">Email:</label>\r\n                <input type=\"email\" class=\"form-control\" id=\"inputEmail4\" placeholder=\"Email\">\r\n              </div>\r\n              <div class=\"form-group col-md-6\">\r\n                <label for=\"inputPassword4\">Password:</label>\r\n                <input type=\"password\" class=\"form-control\" id=\"inputPassword4\" placeholder=\"Password\">\r\n              </div>\r\n            </div>\r\n      </form>\r\n    </div>\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Home Of The Best';
+    }
+    AppComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__("../../../../../src/app/app.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_platform_browser_animations__ = __webpack_require__("../../../platform-browser/esm5/animations.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_platform_browser_animations__["a" /* BrowserAnimationsModule */]
+            ],
+            providers: [],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var environment = {
+    production: false
+};
+
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/esm5/platform-browser-dynamic.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
